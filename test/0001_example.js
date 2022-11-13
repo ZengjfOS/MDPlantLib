3ac4082e513946187093f40ae839a5678d52c981
zengjf <zengjf42@163.com>
2022-11-13 14:42:08 +0800

    mocha before and after

--- test/0001_example.js
+++ test/0001_example.js
@@ -1,11 +1,26 @@
 const assert = require('assert')
 
 describe("test example", function() {
+
+    let globalVar = "global variable"
+
+    before(() => {
+        console.log("do before operation")
+        console.log(globalVar)
+    })
+
+    after(() => {
+        console.log("do after operation")
+        console.log(globalVar)
+    })
+
     it('check index', () => {
         assert.equal(-1, [1, 2, 3].indexOf(5))
+        console.log("check " + globalVar)
     })
 
     it('check value', () => {
         assert.equal(1, 1)
+        console.log("check " + globalVar)
     })
 })
